8c69532d677a9f766faec837d58bd178dfd22fe6
Andherson Castro <agcl82@gmail.com>
2025-04-19 20:21:14 -0400

    Postal Basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Post from "./components/Post";
+
+const App = () => {
+  return <Post />;
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+        <p>Andherson Castro</p>
+        <p>I love React.js!</p>
+    </div>
+  )
+}
+
+export default Post;

fb2c6be99783a759162a387d1e26550df75d7ee6
Andherson Castro <agcl82@gmail.com>
2025-04-20 22:39:43 -0400

    Testing some props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Post from "./components/Post";
 
 const App = () => {
-  return <Post />;
+  return (
+  <>
+    <Post author="Pepe Grillo" message="Nunca digas Nunca" />
+    <Post author="Estopa" message="Esto es Estopa" />
+    <Post author="Jose Panchito" message="Tijale!" />
+  </>
+  ) 
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Post = () => {
+const Post = (props) => {
+
   return (
     <div>
-        <p>Andherson Castro</p>
-        <p>I love React.js!</p>
+        <p>{props.author}</p>
+        <p>{props.message}</p>
     </div>
   )
 }

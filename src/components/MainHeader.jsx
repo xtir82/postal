2a71391b035e657dbb9c13cfb5bb83836adbb78a
agcl82 <agcl82@gmail.com>
2025-04-24 23:04:19 -0400

    Adding Header files

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { MdPostAdd, MdMessage } from 'react-icons/md'
+import styles from './MainHeader.module.css'
+
+const MainHeader = ({ onCreatePost }) => {
+  return (
+    <header className={styles.header}>
+        <h1 className={styles.logo}>
+            <MdMessage />
+            React Poster
+        </h1>
+        <p>
+            <button className={styles.button} onClick={onCreatePost}>   
+                <MdPostAdd size={18} /> 
+                New Post
+            </button>
+        </p>
+
+    </header>
+  )
+}
+
+export default MainHeader;
